9545ac991b9f75caa354e6621d4a52075d743064
hlerda1 <horacio.lerda@gmail.com>
2022-12-17 10:52:20 -0300

    04 - Main page and providers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Layout from "./shared/components/Layout";
+import Home from "./shared/pages/Home";
+import Products from './shared/pages/Products';
+import Product from './shared/pages/Product';
+import 'App.css';
+import EditProduct from "Products/pages/EditProduct";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/products/add" element={<Product/>} />
+        <Route path="/products/edit/:productId" element={<EditProduct />} />
+      </Routes>
+    </Layout>
   );
 }
 
