0482db84da7fc5f1bf365ed48795c418cc5534a7
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-12-17 16:19:56 -0300

    Add providers redux store

---
+++ src/Providers/store/reducer.js
@@ -0,0 +1,42 @@
+const { ProvidersActions } = require('./types');
+
+const INITIAL_STATE = {
+  providers: [],
+  isLoading: false,
+  error: null,
+  provider: null
+};
+
+const providersReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case ProvidersActions.SET_LOADING:
+      return {
+        ...state,
+        isLoading: action.payload,
+      };
+    case ProvidersActions.DISMISS_LOADING:
+      return {
+        ...state,
+        isLoading: action.payload,
+      };
+    case ProvidersActions.SET_PROVIDERS:
+      return {
+        ...state,
+        providers: action.payload,
+      };
+    case ProvidersActions.SET_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case ProvidersActions.SET_PROVIDER:
+      return {
+        ...state,
+        provider: action.payload
+      }
+    default:
+      return state;
+  }
+};
+
+export default providersReducer;
---
+++ src/Providers/store/types.js
@@ -0,0 +1,7 @@
+export const ProvidersActions = Object.freeze({
+  SET_LOADING: 'providers/setLoading',
+  SET_PROVIDERS: 'providers/setProviders',
+  DISMISS_LOADING: 'providers/dismissLoading',
+  SET_ERROR: 'providers/setError',
+  SET_PROVIDER: 'providers/setProvider'
+});
